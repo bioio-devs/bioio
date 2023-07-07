8a0f58b6e544a430393c5a34d43a70eaa2414b8b
sean meharry <seanme97@gmail.com>
2023-07-07 10:11:20 -0700

    Move OME utilities to separate file

--- bioio/bio_image.py
+++ bioio/bio_image.py
@@ -11,6 +11,7 @@ import numpy as np
 import xarray as xr
 from ome_types import OME
 
+from .ome_utils import generate_ome_channel_id
 from .plugins import plugins_by_ext
 
 ###############################################################################
@@ -176,36 +177,6 @@ class BioImage(biob.image_container.ImageContainer):
             ),
         )
 
-    @staticmethod
-    def _generate_ome_channel_id(image_id: str, channel_id: Union[str, int]) -> str:
-        """
-        Naively generates the standard OME channel ID using a provided ID.
-
-        Parameters
-        ----------
-        image_id: str
-            An image id to pull the image specific index from.
-            See: `generate_ome_image_id` for more details.
-        channel_id: Union[str, int]
-            A string or int representing the ID for a channel.
-            In the context of the usage of this function, this is usually used with the
-            index of the channel.
-
-        Returns
-        -------
-        ome_channel_id: str
-            The OME standard for channel IDs.
-
-        Notes
-        -----
-        ImageIds are usually: "Image:0", "Image:1", or "Image:N",
-        ChannelIds are usually the combination of image index + channel index --
-        "Channel:0:0" for the first channel of the first image for example.
-        """
-        # Remove the prefix 'Image:' to get just the index
-        image_index = image_id.replace("Image:", "")
-        return f"Channel:{image_index}:{channel_id}"
-
     def __init__(
         self,
         image: biob.types.ImageLike,
@@ -349,7 +320,7 @@ class BioImage(biob.image_container.ImageContainer):
         # Add channel coordinate plane because it is required in BioImage
         if biob.dimensions.DimensionNames.Channel not in coords:
             coords[biob.dimensions.DimensionNames.Channel] = [
-                BioImage._generate_ome_channel_id(
+                generate_ome_channel_id(
                     image_id=self.current_scene,
                     channel_id=0,
                 )

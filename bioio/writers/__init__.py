ccedc9b0e17f7074f6995d9fb5022b5a647d1f9a
toloudis <danielt@alleninstitute.org>
2024-07-02 11:42:29 -0700

    deprecate the old zarr writer for a sec

--- bioio/writers/__init__.py
+++ bioio/writers/__init__.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from .ome_tiff_writer import OmeTiffWriter
-from .ome_zarr_writer import OmeZarrWriter
+from .ome_zarr_writer import OmeZarrWriter as OmeZarrWriter_old
+from .ome_zarr_writer_2 import OmeZarrWriter
 
 __all__ = [
     "OmeTiffWriter",
     "OmeZarrWriter",
+    "OmeZarrWriter_old",
 ]
